d2a467c023788c50b7db2511ace4263b755f8569
Peter-Haung888 <ming31437@gmail.com>
2023-07-28 16:19:16 +0800

    feat: add login and register route and page

--- app.js
+++ app.js
@@ -11,6 +11,27 @@ app.use(methodOverride('method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
