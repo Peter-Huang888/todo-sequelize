15700be75554c6067f0f1566fdaf7dc2ec160433
Peter-Haung888 <ming31437@gmail.com>
2023-08-02 11:40:58 +0800

    feat:add message partials

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ module.exports = {
     if (req.isAuthenticated()) {
       return next()
     }
+    req.flash('error', '請先登入才能使用！')
     res.redirect('/users/login')
   }
 }
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,13 +8,24 @@ const db = require('../../models')
 const User = db.User
 
 router.get('/login', (req, res) => {
-  res.render('login')
+  const userInput = req.session.userInput || {}
+  delete req.session.userInput
+  const login_error_msg = req.flash('error')
+  return res.render('login', {
+    login_error_msg,
+    email: userInput.email
+  })
 })
-
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
+//加入 middleware , 驗證 request 登入狀態
+router.post('/login',
+  (req, res, next) => {
+    req.session.userInput = req.body
+    next()
+  }, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  }))
 
 router.get('/register', (req, res) => {
   res.render('register')
@@ -22,18 +33,26 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-
+  const errors = []
+  if (!name | !email | !password | !confirmPassword) {
+    errors.push({ message: `所有欄位都是必填。` })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: `密碼與確認密碼不相符!` })
+  }
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: `這個Email已經註冊過了` })
+    }
+    if (errors.length) {
       return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
     }
-
     return bcrypt
       .genSalt(10)
       .then(salt => bcrypt.hash(password, salt))
@@ -49,6 +68,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出')
   res.redirect('/users/login')
 })
 
